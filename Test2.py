eecf88306a1f0bf48ce9a0c64cc23b54ef6fe40a
pallavjitmadhukul-hue <pallavjit.madhukul@gohighlevel.com>
2025-11-24 14:02:33 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -32,7 +32,7 @@ with header:
     col1, col2 = st.columns([1, 8])
 
     with col1:
-        st.image("logo.png", width=70)
+        st.image("logo2.png", width=70)
 
     with col2:
         st.markdown(
